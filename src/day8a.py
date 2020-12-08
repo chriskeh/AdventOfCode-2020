e3b0fefac3eaa7d05f194732bcb8464e80edd60d
Christian Kehrmann <christian.kehrmann@advantest.com>
2020-12-08 10:50:30 +0100

    Day8a with a method

--- src/day8a.py
+++ src/day8a.py
@@ -23,6 +23,45 @@ def slurp_input(input_file):
 
     return my_instructions, my_operands
 
+def run_program(instruction, operand):
+    """
+    Execute the program which is given by two lists: one with the commands, the second with the operands.
+    :param instruction: list with instructions
+    :param operand: list with operands
+    :return: the result in the accumulator
+    """
+
+    # Create a third list that holds the number of executions of the command in each line. Initially 0
+    # Could be improved with True and False ?!?
+    execution = [False for i in range(0, len(instruction))]
+
+    accumulator = 0
+    instruction_counter = 0
+    while True:
+        if instruction_counter == len(instruction):
+            print("Yesss, program terminates")
+            break
+        if execution[instruction_counter]:
+            print("Exiting the loop")
+            break
+        if instruction[instruction_counter] == "nop":
+            execution[instruction_counter] = True
+            instruction_counter += 1
+            print("NOP")
+        elif instruction[instruction_counter] == "acc":
+            execution[instruction_counter] = True
+            accumulator += operand[instruction_counter]
+            instruction_counter += 1
+            print("ACC, sum: {}".format(accumulator))
+        elif instruction[instruction_counter] == "jmp":
+            execution[instruction_counter] = True
+            instruction_counter += operand[instruction_counter]
+            print("JMP: counter: {}".format(instruction_counter))
+        else:
+            exit("invalid instruction {}".format(instruction[instruction_counter]))
+
+    return(accumulator)
+
 
 def main():
 
@@ -33,38 +72,10 @@ def main():
     # print(all_instructions)
     # print(all_operands)
 
-    # Create a third list that holds the number of executions of the command in each line. Initially 0
-    # Could be improved with True and False ?!?
-    execution = [False for i in range(0, len(all_instructions))]
 
-    accumulator = 0
+    result = run_program(all_instructions, all_operands)
 
-    # result = run_program(all_instructions, all_operands)
-    instruction_counter = 0
-    while True:
-        if instruction_counter == len(all_instructions):
-            print("Yesss, program terminates")
-            break
-        if execution[instruction_counter]:
-            print("Exiting the loop")
-            break
-        if all_instructions[instruction_counter] == "nop":
-            execution[instruction_counter] = True
-            instruction_counter += 1
-            print("NOP")
-        elif all_instructions[instruction_counter] == "acc":
-            execution[instruction_counter] = True
-            accumulator += all_operands[instruction_counter]
-            instruction_counter += 1
-            print("ACC, sum: {}".format(accumulator))
-        elif all_instructions[instruction_counter] == "jmp":
-            execution[instruction_counter] = True
-            instruction_counter += all_operands[instruction_counter]
-            print("JMP: counter: {}".format(instruction_counter))
-        else:
-            exit("invalid instruction {}".format(all_instructions[instruction_counter]))
-
-    print("Accumulator: {}".format(accumulator))
+    print("Accumulator: {}".format(result))
     # print(len(all_instructions), len(all_operands), len(execution))
 
     # print("Total: {}".format(total))
