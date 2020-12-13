76259bf5f8e558bc44b88ffb914daf33cb414aae
Christian Kehrmann <christian.kehrmann@advantest.com>
2020-12-13 21:15:04 +0100

    Try for Day13b: runs already several minutes, maybe infinite; Output of Day13a commented; Tests are all green

--- src/day13a.py
+++ src/day13a.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env /usr/bin/python36
+from pprint import pprint
 
 input_data_file = "day13.data"
 
@@ -11,8 +12,6 @@ def slurp_input(input_file):
         1) the earliest departure
         2) A list of sets. Each set represents the given answers in a group.
     """
-    my_answers = []
-    this_answer = set()
 
     with open(input_file, 'r') as f:
         point_in_time = int(f.readline().rstrip())
@@ -23,6 +22,26 @@ def slurp_input(input_file):
         elems = [int(x) for x in elems]
 
     return point_in_time, elems
+
+
+def slurp_input_b(input_file):
+    """
+    Read the input file.
+    :param input_file:
+    :return: A dict of offsets for each bus
+    """
+    offset = dict()
+    index = 0
+    with open(input_file, 'r') as f:
+        ignore = int(f.readline().rstrip())
+        lines_raw = f.readline().rstrip()
+        elems = lines_raw.split(',')
+        for elem in elems:
+            if elem != 'x':
+                offset[int(elem)] = index
+            index += 1
+
+    return int(elems[0]), offset
 
 def calculate_departure(moment, list_of_lines):
     """
@@ -47,18 +66,50 @@ def calculate_departure(moment, list_of_lines):
     return my_line, shortest_waittime
 
 
+def calculate_day13b(first_bus, start_in_time, offsets):
+    """
+    Method for Day13 b
+
+    :param first_bus: The number of the first bus
+    :param start_in_time: The starting point (this is handy for testing vs production)
+    :param offsets: the dictionary that holds the offsets for all given busses
+                The bus numbe is the key and the offset is its value
+    :return: The first point in time that matches
+    """
+
+    current_time = start_in_time
+    while True:
+        match = True
+        for key, value in offsets.items():
+            if (current_time + value) % key != 0:
+                match = False
+                break
+        if match:
+            return(current_time)
+
+        current_time += first_bus
+
+
 def main():
 
     # uncomment the next line to read the input data from the test file
     # input_data_file = "day13_test.data"
 
 
-    earliest_time_to_leave, all_buslines = slurp_input(input_data_file)
+    # earliest_time_to_leave, all_buslines = slurp_input(input_data_file)
+    #
+    # line_id, waittime = calculate_departure(earliest_time_to_leave, all_buslines)
+    # result = line_id * waittime
+    # print("Bus: {}, waittime: {}, result: {}".format(line_id, waittime, result))
 
-    line_id, waittime = calculate_departure(earliest_time_to_leave, all_buslines)
-    result = line_id * waittime
-    print("Bus: {}, waittime: {}, result: {}".format(line_id, waittime, result))
+    first_bus, offsets = slurp_input_b(input_data_file)
+    pprint(offsets)
 
+    start_in_time = 100000000000000
+    # start_in_time = 0
+
+    timestamp = calculate_day13b(first_bus, start_in_time, offsets)
+    print(timestamp)
     # print("Total: {}".format(total))
 
 
